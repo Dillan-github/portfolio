767b05d6653c686c38dba6751842ab5f768f0e69
Dillan Wilken <dillanaidenwilken@gmail.com>
2024-10-20 23:12:28 +0400

    mobile icon size adjustment

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -5,7 +5,7 @@ import { useTheme } from '../../App';
 import white_logo from '../../images/White_Logo_SVG.svg';
 import black_logo from '../../images/Black_Logo_SVG.svg';
 import sun_icon from '../../images/sun_icon.svg';
-import moon_icon from '../../images/moon_icon.svg';
+import moon_icon from '../../images/moon_icon.png';
 import hamburger_menu from '../../images/hamburger_menu.svg';
 
 
@@ -17,9 +17,9 @@ function NavBar ({onClick}) {
     return(
     <div id='nav-bar'>
         <img src={theme === 'dark' ? white_logo : black_logo} id="nav-logo" alt="logo" />
-        <div>
-        <img  src={theme === 'dark' ? sun_icon : moon_icon} id="theme-icon" alt="theme button" onClick={toggleTheme} />
-        <img src={hamburger_menu} id="hamburger" alt="menu" />
+        <div id='icon-menu-container'>
+            <img  src={theme === 'dark' ? sun_icon : moon_icon} id="theme-icon" alt="theme button" onClick={toggleTheme} />
+            <img src={hamburger_menu} id="hamburger" alt="menu" />
         </div>
     </div>
     );
